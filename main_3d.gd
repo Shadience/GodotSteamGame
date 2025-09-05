4051c355801acbcad9aea60b7d5494657d8e38dc
Shadience <rusibatullin08@mail.ru>
2025-09-05 16:44:29 +0400

    Update main_3d.gd

--- main_3d.gd
+++ main_3d.gd
@@ -6,7 +6,6 @@ var steam_id = 0
 var peer := SteamMultiplayerPeer.new()
 @onready var spawner: MultiplayerSpawner = $MultiplayerSpawner
 
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
@@ -18,17 +17,21 @@ func _ready():
 	if Steam.getPersonaName() == "абдвг1848":
 		Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, 4)
 		peer.create_host()
-	else:
-		peer.create_client(steam_id)
-	multiplayer.multiplayer_peer = peer
 
 func lc(connec, lobby_id):
 	Steam.allowP2PPacketRelay(true)
+	multiplayer.multiplayer_peer = peer
 
 func lj(lobby, perms, locked, resp):
 	spawner.spawn(steam_id)
 	set_multiplayer_authority(steam_id, true)
 	print("PRIKOL")
+
+func jr(lobby, id):
+	Steam.joinLobby(lobby)
+	peer.create_client(id)
+	peer.connect_to_lobby(lobby)
+	multiplayer.multiplayer_peer = peer
 
 func _spawn_player(id):
 	var p = load(spawner.get_spawnable_scene(0)).instantiate()
