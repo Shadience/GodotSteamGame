3e5d6b89638d8a8bd9df38921a8527c36e8d31c3
Shadience <rusibatullin08@mail.ru>
2025-09-05 16:31:06 +0400

    Update main_3d.gd

--- main_3d.gd
+++ main_3d.gd
@@ -2,12 +2,37 @@ extends Node3D
 
 var captured = true;
 
+var n = 0
 var steam_id = 0
 var peer := SteamMultiplayerPeer.new()
+@onready var spawner: MultiplayerSpawner = $MultiplayerSpawner
+
 
 func _ready():
-	steam_id = Steam.getSteamID()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	steam_id = Steam.getSteamID()
+	Steam.lobby_created.connect(lc)
+	Steam.lobby_joined.connect(lj)
+	spawner.spawn_function = _spawn_player
+	
+	if Steam.getPersonaName() == "абдвг1848":
+		Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, 4)
+		peer.create_host()
+
+func lc(connec, lobby_id):
+	multiplayer.multiplayer_peer = peer
+	Steam.allowP2PPacketRelay(true)
+
+func lj(lobby, perms, locked, resp):
+	spawner.spawn(steam_id)
+	set_multiplayer_authority(steam_id, true)
+
+func _spawn_player(id):
+	var p = load(spawner.get_spawnable_scene(0)).instantiate()
+	p.set_multiplayer_authority(id)
+	#n += 1
+	return p
 
 func _unhandled_input(event: InputEvent):
 	if event.is_pressed():
